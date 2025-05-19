5f6ee3685353ff5280bb84774f6f5821b4d36da4
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-05-20 00:13:12 +0800

    add login and signup button

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,47 +1,72 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router';
+import { Link, NavLink } from "react-router";
 
 const Navbar = () => {
-    const links=<>
-    <li>
-        <NavLink to='/'>Home</NavLink>  
-    </li>
-    <li>
-    <NavLink to='/all-groups'>All Groups</NavLink>
-    </li>
-    <li>
-    <NavLink to='/create-group'>Create Groups</NavLink>
-    </li>
-    <li>
-    <NavLink to='/my-groups'>My Groups</NavLink>
-    </li>
-    
+  const links = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/all-groups">All Groups</NavLink>
+      </li>
+      <li>
+        <NavLink to="/create-group">Create Groups</NavLink>
+      </li>
+      <li>
+        <NavLink to="/my-groups">My Groups</NavLink>
+      </li>
     </>
-    return (
-        <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+  );
+ 
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {links}
+          </ul>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-    {links}
-      </ul>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+      <div className="navbar-end flex gap-2">
+       <Link className="btn btn-active btn-primary" to="/login">
+              Login
+            </Link>
+            <Link className="btn btn-active btn-primary" to="/signup">
+              Sign Up
+            </Link>
+      
+
+        <div className="">
+          <div className="avatar avatar-online w-[60px] ">
+           
+          </div>
+        </div>
+      </div>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-     {links}
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Navbar;
