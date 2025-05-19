7d3556ec507a46240024dfcab5b92350990b8040
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-05-20 00:00:59 +0800

    firebase installation and setup private route with auth provider

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCH5ZPOXPkD316BfkXn5ISR0c5Y05CafCs",
+  authDomain: "hobby-hub-ef096.firebaseapp.com",
+  projectId: "hobby-hub-ef096",
+  storageBucket: "hobby-hub-ef096.firebasestorage.app",
+  messagingSenderId: "612694425227",
+  appId: "1:612694425227:web:bde42c8cec69e57758de8e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
