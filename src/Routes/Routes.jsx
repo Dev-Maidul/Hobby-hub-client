1737c9572e00e3e4a9cf86940127b6abb525a8c0
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-05-19 23:53:23 +0800

    add dynmic routes in navbar

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,11 @@ import {
 } from "react-router";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home";
+import AllGroup from '../Pages/AllGroup';
+import CreateGroup from '../Pages/CreateGroup';
+import MyGroups from '../Pages/MyGroups';
+import Login from '../Components/Login';
+import Signup from '../Components/Signup';
 
 export const router = createBrowserRouter([
   {
@@ -13,7 +18,27 @@ export const router = createBrowserRouter([
         {
             index:true,
             Component:Home
-        }
+        },
+        {
+            path:'/all-groups',
+            Component:AllGroup
+        },
+        {
+            path:'/create-group',
+            Component:CreateGroup
+        },
+        {
+            path:'/my-groups',
+            Component:MyGroups
+        },
+        {
+            path:'/login',
+            Component:Login
+        },
+        {
+            path:'/signup',
+            Component:Signup
+        },
     ]
   },
 ]);
