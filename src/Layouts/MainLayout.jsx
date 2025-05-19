67c2c7a6734684cf27919fb49c8ab5d53e7be676
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-05-19 23:05:27 +0800

    Navbar and footer added

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,15 +1,18 @@
+
 import React from 'react';
 
 import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
+import Navbar from '../Components/Navbar';
 
 
 
 const MainLayout = () => {
     return (
         <div className='w-10/12 mx-auto'>
-           
+           <Navbar></Navbar>
            <Outlet></Outlet>
-           
+           <Footer></Footer>
         </div>
     );
 };
